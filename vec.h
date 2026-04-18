bbf9596da991552e6d6bb09c133c235c3d6d2b69
galag <gal.granot95@gmail.com>
2026-04-18 22:55:47 +0300

    generic

--- vec.h
+++ vec.h
@@ -6,55 +6,101 @@
 
 #define VEC_INITIAL_CAPACITY 4
 
-typedef struct {
-    int *data;
-    size_t capacity;
-} IntSlice;
+#define VEC_IMPL(T, Name) \
+typedef struct { \
+    T *data; \
+    size_t capacity; \
+} Name##Slice; \
+\
+typedef struct { \
+    Name##Slice slice; \
+    size_t len; \
+} Name; \
+\
+static inline Name Name##_new(void) { \
+    return (Name){ \
+        .slice = (Name##Slice){ .data = NULL, .capacity = 0 }, \
+        .len = 0 \
+    }; \
+} \
+\
+static inline void Name##_initial_alloc(Name *vec) { \
+    vec->slice.data = malloc(VEC_INITIAL_CAPACITY * sizeof(T)); \
+    vec->slice.capacity = VEC_INITIAL_CAPACITY; \
+} \
+\
+static inline void Name##_double_capacity(Name *vec) { \
+    size_t new_capacity = vec->slice.capacity * 2; \
+    void *new_data = realloc(vec->slice.data, new_capacity * sizeof(T)); \
+    vec->slice.data = new_data; \
+    vec->slice.capacity = new_capacity; \
+} \
+\
+static inline void Name##_push(Name *vec, T value) { \
+    if (!vec->slice.data) { \
+        Name##_initial_alloc(vec); \
+    } else if (vec->len == vec->slice.capacity) { \
+        Name##_double_capacity(vec); \
+    } \
+    vec->slice.data[vec->len++] = value; \
+} \
+\
+static inline T Name##_get(const Name *vec, size_t i) { \
+    return vec->slice.data[i]; \
+} \
+\
+static inline void Name##_free(Name *vec) { \
+    free(vec->slice.data); \
+    vec->slice.data = NULL; \
+    vec->slice.capacity = 0; \
+    vec->len = 0; \
+}
 
 typedef struct {
-    IntSlice slice;
-    size_t len;
+  int *data;
+  size_t capacity;
+} IntVecSlice;
+
+typedef struct {
+  IntVecSlice slice;
+  size_t len;
 } IntVec;
 
-static inline void int_vec_double_capacity(IntVec *vec) {
-    size_t new_capacity = vec->slice.capacity * 2;
-    void *new_data = realloc(vec->slice.data, new_capacity * sizeof(int));
-    *vec = (IntVec) {
-        .slice = (IntSlice) { .data = new_data, .capacity = new_capacity },
-        .len = vec->len,
-    };
+static inline IntVec IntVec_new(void) {
+  return (IntVec){.slice = (IntVecSlice){.data = ((void *)0), .capacity = 0},
+                  .len = 0};
 }
 
-static inline void int_vec_initial_alloc(IntVec *vec) {
-    vec->slice = (IntSlice) { 
-        .data = malloc(VEC_INITIAL_CAPACITY * sizeof(int)),
-        .capacity = VEC_INITIAL_CAPACITY,
-    };
+static inline void IntVec_initial_alloc(IntVec *vec) {
+  vec->slice.data = malloc(4 * sizeof(int));
+  vec->slice.capacity = 4;
 }
 
-static inline IntVec int_vec_new(void) {
-    return (IntVec) {
-        .slice = (IntSlice) { .data = NULL, .capacity = 0, },
-        .len = 0,
-    };
+static inline void IntVec_double_capacity(IntVec *vec) {
+  size_t new_capacity = vec->slice.capacity * 2;
+  void *new_data = realloc(vec->slice.data, new_capacity * sizeof(int));
+  vec->slice.data = new_data;
+  vec->slice.capacity = new_capacity;
 }
 
-static inline void int_vec_push(IntVec *vec, int value) {
-    if(!vec->slice.data) {
-        int_vec_initial_alloc(vec);
-    } else if(vec->len == vec->slice.capacity) {
-        int_vec_double_capacity(vec);
-    }
-    vec->slice.data[vec->len] = value;
-    vec->len += 1;
+static inline void IntVec_push(IntVec *vec, int value) {
+  if (!vec->slice.data) {
+    IntVec_initial_alloc(vec);
+  } else if (vec->len == vec->slice.capacity) {
+    IntVec_double_capacity(vec);
+  }
+  vec->slice.data[vec->len++] = value;
 }
 
-static inline int int_vec_get(const IntVec *vec, size_t i) {
-    return vec->slice.data[i];
+static inline int IntVec_get(const IntVec *vec, size_t i) {
+  return vec->slice.data[i];
 }
 
-static inline void int_vec_free(IntVec *vec) {
-    free(vec->slice.data);
+static inline void IntVec_free(IntVec *vec) {
+  free(vec->slice.data);
+  vec->slice.data = ((void *)0);
+  vec->slice.capacity = 0;
+  vec->len = 0;
 }
 
 #endif // VEC_H
